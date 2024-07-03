6ea4521a464c261974e0960ab4b977806ba2af9e
Luiz Felipe Reis <felipe1737@localhost.localdomain>
2024-07-02 21:44:47 -0300

    feat

---
+++ sql/schema.sql
@@ -0,0 +1,21 @@
+create database service_queue;
+
+create table queue(
+    id serial primary key,
+    name varchar(255) not null,
+    preferential boolean default false not null,
+    open_service boolean default true not null,
+    queue_started timestamp default now() not null,
+    service_started timestamp default null,
+    service_finished timestamp default null
+);
+
+create table operator(
+    id serial primary key,
+    name varchar(255) not null,
+    email varchar(255) not null unique,
+    password varchar(255) not null,
+    ticket_window integer unique not null,
+    open_service boolean default false not null,
+    
+);
---
+++ src/router/router.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { createServiceQueue, getServiceQueue } from "../controllers/serviceQueue";
+import { validationNameServiceQueue } from "../middleware/queue";
+import { createOperators, loginOperator, updateOperators } from "../controllers/operators";
+import { validationEmailOperator, validationTicketWindow } from "../middleware/operator";
+import { loginValidation } from "../jwt/jwtTokenUser";
+
+const router = Router();
+
+export default router;
+
+router.post("/login", loginOperator);
+
+router.post("/queue", createServiceQueue);
+
+router.use(loginValidation);
+
+router.get("/queue", validationNameServiceQueue, getServiceQueue);
+
+router.post("/operator", validationTicketWindow, validationEmailOperator, createOperators);
+router.put("/operator", validationTicketWindow, validationEmailOperator, updateOperators);
